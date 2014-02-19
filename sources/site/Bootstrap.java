99c0910ffb95c67861ea0fb7e0a14532b0dfa416
vagrant <vagrant@build>
2014-02-19 07:43:09 +0100

    revert to time

--- sources/site/Bootstrap.java
+++ sources/site/Bootstrap.java
@@ -17,7 +17,7 @@ class Bootstrap {
             public void handle(String target, Request baseRequest,
                 HttpServletRequest request, HttpServletResponse response)
                 throws IOException, ServletException {
-                if (target.startsWith("/datetime")) {
+                if (target.startsWith("/time")) {
                     response.setHeader("Content-Type", "text/html");
                     response.getWriter().write("<p>" + new Date());
                     baseRequest.setHandled(true);
